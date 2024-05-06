56f9eca0892b76a0cab72e1ed46da9bbed1be560
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2024-05-06 10:48:45 +0200

    UF-8205: Add logic for POB-calls

---
+++ src/main/java/se/sundsvall/incidentmapper/service/IncidentService.java
@@ -0,0 +1,43 @@
+package se.sundsvall.incidentmapper.service;
+
+import static java.time.OffsetDateTime.now;
+import static se.sundsvall.incidentmapper.integration.db.model.enums.Status.POB_INITIATED_EVENT;
+import static se.sundsvall.incidentmapper.integration.db.model.enums.Status.SYNCHRONIZED;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import se.sundsvall.incidentmapper.api.model.IncidentRequest;
+import se.sundsvall.incidentmapper.integration.db.IncidentRepository;
+import se.sundsvall.incidentmapper.integration.db.model.IncidentEntity;
+import se.sundsvall.incidentmapper.integration.db.model.enums.Status;
+
+@Service
+@Transactional
+public class IncidentService {
+
+	private static final List<Status> OPEN_FOR_MODIFICATION_STATUS_LIST = List.of(null, SYNCHRONIZED);
+
+	private final IncidentRepository incidentRepository;
+
+	public IncidentService(IncidentRepository incidentRepository) {
+		this.incidentRepository = incidentRepository;
+	}
+
+	public void handleIncidentRequest(IncidentRequest request) {
+
+		final var issueKey = request.getIncidentKey();
+		final var incidentEntity = incidentRepository.findByPobIssueKey(issueKey)
+			.orElse(IncidentEntity.create().withPobIssueKey(issueKey));
+
+		if (OPEN_FOR_MODIFICATION_STATUS_LIST.contains(incidentEntity.getStatus())) {
+			incidentEntity.withStatus(POB_INITIATED_EVENT);
+		}
+
+		incidentEntity.withPobIssueLastModified(now());
+
+		incidentRepository.save(incidentEntity);
+	}
+}

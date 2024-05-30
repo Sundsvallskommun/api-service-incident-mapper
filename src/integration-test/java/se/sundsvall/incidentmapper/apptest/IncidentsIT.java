79ee5caa6368a2c9d2ebe42e0b8af8481be9fade
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2024-05-30 10:03:20 +0200

    UF-8140: Added 1 more IT-test

--- src/integration-test/java/se/sundsvall/incidentmapper/apptest/IncidentsIT.java
+++ src/integration-test/java/se/sundsvall/incidentmapper/apptest/IncidentsIT.java
@@ -2,6 +2,7 @@ package se.sundsvall.incidentmapper.apptest;
 
 import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.http.HttpStatus.ACCEPTED;
+import static se.sundsvall.incidentmapper.integration.db.model.enums.Status.JIRA_INITIATED_EVENT;
 import static se.sundsvall.incidentmapper.integration.db.model.enums.Status.SYNCHRONIZED;
 
 import org.junit.jupiter.api.Test;
@@ -45,6 +46,26 @@ class IncidentsIT extends AbstractAppTest {
 			.withJiraIssueKey("UF-5974")
 			.withStatus(SYNCHRONIZED));
 
+		setupCall()
+			.withServicePath("/incidents")
+			.withHttpMethod(POST)
+			.withRequest(REQUEST_FILE)
+			.withExpectedResponseStatus(ACCEPTED)
+			.withMaxVerificationDelayInSeconds(5)
+			.sendRequest()
+			.verifyStubs();
+	}
+
+	@Test
+	void test03_updatedIssueFromJira() {
+
+		incidentRepository.saveAndFlush(IncidentEntity.create()
+			.withPobIssueKey("12345")
+			.withJiraIssueKey("UF-5974")
+			.withStatus(JIRA_INITIATED_EVENT));
+
+		// Not necessary for this test case, but must be called in order to call verifyStubs().
+		// The updatePob-logic is a scheduled job and is not depending on any user inputs to this micro-service.
 		setupCall()
 			.withServicePath("/incidents")
 			.withHttpMethod(POST)

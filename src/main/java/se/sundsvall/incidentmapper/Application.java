dcc600e6a03f2854cf9f844ca0b159a897c50d9b
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2024-11-25 12:14:29 +0100

    Fixed Shedlock bean load issue.

--- src/main/java/se/sundsvall/incidentmapper/Application.java
+++ src/main/java/se/sundsvall/incidentmapper/Application.java
@@ -4,8 +4,6 @@ import static org.springframework.boot.SpringApplication.run;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import se.sundsvall.dept44.ServiceApplication;
 import se.sundsvall.dept44.util.jacoco.ExcludeFromJacocoGeneratedCoverageReport;
 
@@ -30,7 +28,6 @@ import se.sundsvall.dept44.util.jacoco.ExcludeFromJacocoGeneratedCoverageReport;
 @ServiceApplication
 @EnableFeignClients
 @EnableScheduling
-@EnableSchedulerLock(defaultLockAtMostFor = "PT10M")
 @ExcludeFromJacocoGeneratedCoverageReport
 public class Application {
 	public static void main(final String... args) {
--- src/main/java/se/sundsvall/incidentmapper/service/scheduler/ShedLockConfiguration.java
+++ src/main/java/se/sundsvall/incidentmapper/service/scheduler/ShedLockConfiguration.java
@@ -1,16 +1,16 @@
 package se.sundsvall.incidentmapper.service.scheduler;
 
 import javax.sql.DataSource;
-
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import net.javacrumbs.shedlock.core.LockProvider;
-import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
-
 @Configuration
-public class ShedLockConfiguration {
+@EnableSchedulerLock(defaultLockAtMostFor = "PT10M")
+class ShedLockConfiguration {
 
 	@Bean
 	LockProvider lockProvider(DataSource dataSource) {

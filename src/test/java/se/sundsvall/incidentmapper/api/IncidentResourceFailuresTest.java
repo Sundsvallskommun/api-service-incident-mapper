55e7f607a35cc79ffb9b37ee6fefadfd53f5c57a
cheezi0747 <linus.sjolinder@b3.se>
2024-12-10 10:55:55 +0100

    UF-11274: Fix deperacted mockbean

--- src/test/java/se/sundsvall/incidentmapper/api/IncidentResourceFailuresTest.java
+++ src/test/java/se/sundsvall/incidentmapper/api/IncidentResourceFailuresTest.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.zalando.problem.violations.ConstraintViolationProblem;
@@ -29,7 +29,7 @@ class IncidentResourceFailuresTest {
 	@Autowired
 	private WebTestClient webTestClient;
 
-	@MockBean
+	@MockitoBean
 	private IncidentService incidentServiceMock;
 
 	@Test
--- src/test/java/se/sundsvall/incidentmapper/api/IncidentResourceTest.java
+++ src/test/java/se/sundsvall/incidentmapper/api/IncidentResourceTest.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import se.sundsvall.incidentmapper.Application;
@@ -23,7 +23,7 @@ class IncidentResourceTest {
 	@Autowired
 	private WebTestClient webTestClient;
 
-	@MockBean
+	@MockitoBean
 	private IncidentService incidentServiceMock;
 
 	@Test
